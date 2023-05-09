5c2593077e0c8847f32d2336fae9f97480ac4898
Vitor Daniel <vitor036daniel@gmail.com>
2023-05-08 22:00:56 -0300

    fix api

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ const app = express()
 app.use(cors())
 app.use(helmet())
 app.use(morgan("combined"))
-app.use(express.json())
+app.use(express.json({ limit: "100mb" }))
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", async (req, res) => {
